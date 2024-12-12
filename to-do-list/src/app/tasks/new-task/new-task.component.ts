9107f30e833966a709fee73532fb15304240fc79
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2024-12-12 11:26:47 +0200

    adding some functionality to the app

--- to-do-list/src/app/tasks/new-task/new-task.component.ts
+++ to-do-list/src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  enteredTitle = '';
+  enteredText = '';
+@Output() add = new EventEmitter<{title:string, text:string}>();
 
+onSubmit () {
+  this.add.emit({title: this.enteredTitle, text:this.enteredText});
+  this.enteredTitle = '';
+  this.enteredText = '';
+}
 }
--- to-do-list/src/app/tasks/task/task.component.ts
+++ to-do-list/src/app/tasks/task/task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {Task} from './task.model';
 
 @Component({
   selector: 'app-task',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+data = input.required<Task>();
 }
